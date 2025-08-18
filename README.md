30d07e44be67c45d8eabb8155ae74fc750f91447
minwoo <cap019@naver.com>
2025-08-18 22:16:36 +0900

    ci: Spring Boot Gradle CI 워크플로우 추가 (#1)
    * ci: Spring Boot Gradle CI 워크플로우 추가
    
    * test: H2 기반 테스트 프로필 추가 및 contextLoads 안정화
    
    * docs: README.md 업데이트
    
    * ci: 워크플로우 트리거를 pull_request 전용으로 변경
    
    - main 브랜치 직접 push 시 실행되는 CI 트리거 제거
    - pull_request 이벤트에 대해서만 빌드/테스트 실행되도록 설정
    - Ruleset과 충돌하던 불필요한 push 체크 해소
    - 브랜치 main에 큰따옴표 추가

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # study-hub
+
+![CI](https://github.com/Mwoo0383/study-hub/actions/workflows/ci.yml/badge.svg)
---
+++ src/test/java/lang/studyhub/StudyHubApplicationTests.java
@@ -0,0 +1,15 @@
+package lang.studyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class StudyHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
