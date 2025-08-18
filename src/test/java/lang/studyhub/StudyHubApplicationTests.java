e6a63150d0b698ebfb547208e19e5ce288a71738
Minwoo <cap019@naver.com>
2025-08-18 21:06:19 +0900

    ci: Spring Boot Gradle CI 워크플로우 추가

---
+++ src/test/java/lang/studyhub/StudyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package lang.studyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
